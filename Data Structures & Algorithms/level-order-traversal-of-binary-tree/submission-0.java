7dd895aa61f1a675e0243a1c7a84f675d9c5b630
SoundariyaMathavan <soundariyamathavan04@gmail.com>
2026-07-04 11:34:41 +0530

    Add: level-order-traversal-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-0.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> l1 = new ArrayList<>();
+        if(root==null) return l1;   
+        q.add(root);
+        while(!q.isEmpty())
+        {   
+            int size = q.size();
+            List<Integer> l = new ArrayList<>();
+            for(int i=0;i<size;i++){          
+            TreeNode temp = q.poll();
+            l.add(temp.val);
+            if(temp.left!=null) q.add(temp.left);
+            if(temp.right!=null) q.add(temp.right);
+            }
+            l1.add(l);
+        } 
+        
+        return l1;
+    }
+}
